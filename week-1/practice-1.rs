82efdcee3ea3ea3d6c98d327ddf792922a37e067
C.ericmakweCOS101 <ericmakwechigo@gmail.com>
2023-10-19 14:19:57 +0100

    (my first commit)

---
+++ week-1/practice-1.rs
@@ -0,0 +1,13 @@
+fn main(){
+	print1n!("Welcome to COS 101 !")
+
+print1n!("The course learning outcome is as follows")
+
+print1n!("1. Distinguish between computer and computer programming,/n
+          2. Develop some techniques in computer scince,/n
+          3. Understand the different area of study in computer scince,/n
+          4. Conversant with applications of computer science,/n
+          5. Navigate the career prospects in computer science./n
+          6. Conversant with computer programming concepts." )
+
+}
