4f6ae36a1544b7b1d8b034c89cb31d2b43de9bf1
C.ericmakweCOS101 <ericmakwechigo@gmail.com>
2023-10-26 16:57:29 +0100

    week-3

---
+++ week-3/practice_2/target/debug/deps/practice_2.d
@@ -0,0 +1,5 @@
+C:\Users\ericm\Documents\C.ericmakweCOS101\week-3\practice_2\target\debug\deps\practice_2.exe: src\main.rs
+
+C:\Users\ericm\Documents\C.ericmakweCOS101\week-3\practice_2\target\debug\deps\practice_2.d: src\main.rs
+
+src\main.rs:
